cbe2d6a363fe2c3c23c74479027438541081431f
Sanjay peringayil <sanjayperingayil@gmail.com>
2025-08-23 11:01:05 +0530

    prod

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://artigence-task-backend.vercel.app/", // change to your deployed backend URL
+  baseURL: "http://artigence-task-backend.vercel.app", // http://localhost:5000/api change to your deployed backend URL
   headers: {
     "Content-Type": "application/json"
   }
